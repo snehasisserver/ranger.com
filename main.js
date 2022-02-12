20eaa089754e80992206853b9b6bf1fa594fc02c
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-02-12 11:57:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image,function(Img){
+		block_img_object=Img;
+		block_img_object.scaleToWidth(block_image_width);
+		block_img_object.scaleToHeight(block_image_height);
+		block_img_object.set({
+			top:block_y,
+			left:block_x
+		});
+		canvas.add(block_img_object);
+		});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		
+		new_image('rr.jpg');
+		console.log("r for red power ranger");
+	
+	}
+	if(keyPressed == '71')
+	{
+		block_x =200;
+		block_y=50;
+		new_image('gr.png');
+		console.log("g for green power ranger");
+		
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		block_y=50;
+		new_image('yr.png');
+		console.log("y for red power ranger");
+		
+	}
+	if(keyPressed == '80')
+	{
+		block_x =600;
+		block_y=50;
+		new_image('pr.png');
+		console.log("p for red power ranger");
+		
+
+	}
+	if(keyPressed == '66')
+	{
+		block_x =700;
+		block_y=50;
+		new_image('br.png');
+		console.log("b for red power ranger");
+		
+	}
+	
+}
+
